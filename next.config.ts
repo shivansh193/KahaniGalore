646e583a3f7c80fd6e205eaff2343b500a12f85a
shivansh193 <shivanshkalra796@gmail.com>
2025-08-20 22:24:08 +0530

    whoweare and whatwedo created

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rules: {
+    "react/no-unescaped-entities": "off",
+  }
 };
 
 export default nextConfig;
